cb94ac1e9655ee784d137a36b084f45022ee1990
BrunoSegato13 <brunosegato@hotmail.com>
2021-11-10 22:06:44 -0300

    refs# Refactoring Exception

--- src/main/java/com/prova/api/exceptions/CustomException.java
+++ src/main/java/com/prova/api/exceptions/CustomException.java
@@ -1,28 +1,13 @@
 package com.prova.api.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class CustomException {
 
     private Integer status;
     private String message;
 
-    public CustomException(Integer status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
